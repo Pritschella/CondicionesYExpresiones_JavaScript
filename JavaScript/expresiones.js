58e514729893a92ee8dc5317aeab3f969a4a535e
Pritschella <prits99@hotmail.com>
2023-09-06 16:42:39 -0600

    Se agregan los archivos realizados en clase acerca de operadores y expresiones

---
+++ JavaScript/expresiones.js
@@ -0,0 +1,20 @@
+console.log("Estoy vivooooooo");
+
+/*
+Operador
+
+Elemento que nos permite realizar una operación entre dos o más elementos.
+
+- Operadores aritméticos (+, -, *, /)
+- Operadores de asignación (=, ==, === / asignación, igualdad, igualdad estricta o estrictamente igual)
+- Operadores de cadenas (tolowerCase, toUpperCase, trim, toString, concat)
+- Operadores lógicos (&&, ||, !)
+- Operadores de comparación (<, >, <=, >=, ==, ===)
+
+*/
+
+/*Operadores aritméticos
+
+Son operadores que nos permiten tomar valores como sus operandos y retornar un valor numérico único. Corresponden a operaciones matemáticas.
+
+*/
---
+++ JavaScript/operadores.js
@@ -0,0 +1,25 @@
+//Operadore Aritméticos
+
+//Suma (+): Se utiliza para sumar dos números.
+let resultado = 5 + 3; //resultado contendrá el valor 8
+
+//Resta (-): Se utiliza para restar un número de otro.
+let resultado1 =  10 - 4; //resultado contendrá el varo 6. 
+
+//Multiplicación (*): Se utiliza para mutiplicar dos números.
+let resultado2 = 6 * 3; //resultado contendrá el valor 18.
+
+//División (/): Se utiliza para dividir un número por otro. La división por cero produce un valoro especial llamada "Infinity" (infinito).
+let resultado3 = 15 / 3; //resultado contendrá el valor 5
+
+//Módulo (%): Se utiliza para obtener el resto de una división entre dos números.
+let resultado4 = 10 % 3 //resultado contendrá el valor 1 (el resto de 10 dividido por 3)
+
+//Incremento (++): Se utiliza para aumentar en 1 el valor de una variable. 
+let numero = 5;
+numero++;
+//Ahora número contendrá el valor 6
+
+//Decremento (--): Se utiliza para disminuir en 1 el valor de una variable.
+let numero1 = 8;
+numero1--; //ahora número contendrá el valor 7
